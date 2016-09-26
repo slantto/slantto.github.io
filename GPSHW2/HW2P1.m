386a3f8b151de393d1208f46c0f54265aa230a4d
slantto <slantto@mix.wvu.edu>
2016-09-26 13:38:48 -0400

    finish homework 2

--- GPSHW2/HW2P1.m
+++ GPSHW2/HW2P1.m
@@ -1,9 +1,9 @@
-%HW 3 Problem 1
+%HW 2 Problem 1
 %Sean Lantto
 
 clear all
 
-load('dataSet3.mat')
+load('dataSet4.mat')
 
 %Setting Constants
 N=2400; %number of epochs
@@ -30,35 +30,43 @@ tshell=0;
 
 ion=input('Include Ionosphere delay(1 for yes, 0 for no)');
 if ion==1
+    
     ionfree=0;
+    
+    A1=input('define A1(sec)');
+    A2=input('define A2(sec)');
+    A3=input('define A3(sec)');
+    A4=input('define A4(sec)');
+    timeoffset=input('How many hours since begining of gps week?');
+    timeoffset=timeoffset*3600; %convert to seconds
+    
     whichion=input('1 for klorbuchar, 2 for thin shell');
     if whichion==1
         klo=1;
-        A1=input('define A1(sec)');
-        A2=input('define A2(sec)');
-        A3=input('define A3(sec)');
-        A4=input('define A4(sec)');
-        timeoffset=input('How many hours since begining of gps week?');
-        timeoffset=timeoffset*3600; %convert to seconds
         
     elseif whichion==2
         tshell=1;
         
-        
     end
 elseif ion==0
+    TECU=0;
     ionfree=input('Would you like to use the dual-frequency iono-free model?(1=yes, 0=no)');
 end
+
 
 %For P1 data
 for i=1:N
     %Seperate out pseudorange and Satxyz for epoch i
-    pr=prDataP1(1:nSat(i),i);
+    if ionfree==0
+        pr=prDataP1(1:nSat(i),i);
+    elseif ionfree==1
+        pr=(2.546*prDataP1(1:nSat(i),i))-(1.546*prDataP2(1:nSat(i),i));
+    end
     Satxyz=satsXYZ(1:nSat(i),:,i);
     Nsats=nSat(i);
     W=zeros(nSat(i));
     llh=xyz2llh(xyz);
-    if klo==1
+    if ion==1
         t=mod(time(i),86400)+timeoffset;
     else
         t=0;
@@ -72,13 +80,14 @@ for i=1:N
         mel(x)=1/(sqrt(1-((cosd(el(x,i))/1.001).^2))); %mapping function for troposphere delay as a function of elevation angle
     end
     
-    
-    %Use function LLSPos to get xyz estimate at each epoch
-    [xyzEstP1(:,i),clockBiasEstP1(:,i),PDOP(:,i),TDOP(:,i),GDOP(:,i),preFit,postFit]=LLSPos(Satxyz,pr,nomXYZ,clockBiasNom,Nsats,c,W,trop,ion,mel,llh,Po,To,eo,A1,A2,A3,A4,t,el(:,i),klo,tshell,ionfree);
+    %Use function LLSPos to get xyz estimate at each epoch;
+    [xyzEst(:,i),clockBiasEst(:,i),PDOP(:,i),TDOP(:,i),GDOP(:,i),prefit,postFit(:,i),TECU(:,i)]=LLSPos(Satxyz,pr,nomXYZ,clockBiasNom,Nsats,c,W,trop,ion,mel,llh,Po,To,eo,A1,A2,A3,A4,t,el(:,i),klo,tshell,ionfree);
     %substitute xyz estimate for epoch i to be xyz nom for epoch i+1
-    nomXYZ=xyzEstP1(:,i)';
-    clockBiasNom=clockBiasEstP1(:,i)';
-    
+    nomXYZ=xyzEst(:,i)';
+    clockBiasNom=clockBiasEst(:,i)';
+    for x=1:nSat(i)
+        preFit(x,i)=prefit(x);
+    end
     
     %Find Truth ENU
     xyz=truthXYZ(:,i);
@@ -86,18 +95,56 @@ for i=1:N
     enu(:,i)=xyz2enu(xyz,orgxyz);
     truthenu(:,i)=enu(:,i);
     
-    %find ENU 
-    xyz=xyzEstP1(:,i);
+    %find ENU
+    xyz=xyzEst(:,i);
     orgxyz=xyznom;
     enu(:,i)=xyz2enu(xyz,orgxyz);
-    estenuP1(:,i)=enu(:,i);
+    estenu(:,i)=enu(:,i);
     
-   
+    RMSEenu(1,i)=sqrt(mean((estenu(1,i)-truthenu(1,i)).^2));
+    RMSEenu(2,i)=sqrt(mean((estenu(2,i)-truthenu(2,i)).^2));
+    RMSEenu(3,i)=sqrt(mean((estenu(3,i)-truthenu(3,i)).^2));
+    RMSEclkB(i)=sqrt(mean((clockBiasEst(i)-(truthClockBias(i)/c)).^2));
+    RMSE3d(:,i)=sqrt(mean((estenu(:,i)-truthenu(:,i)).^2));
     
 end
 
 %plot enu error
 figure
-plot(1:N,(estenuP1-truthenu))
+diffenu=(estenu-truthenu);
+diffclkB=clockBiasEst-(truthClockBias/c);
+subplot(211)
+plot(1:N,diffenu)
+ylabel('error(m)')
+xlabel('epoch')
+legend('E','N','U')
+title('Estimated ENU error(DataSet4)')
+subplot(212)
+plot(1:N,diffclkB)
+ylabel('error(s)')
+xlabel('epoch')
+title('Estimated clock bias error(DataSet4)')
 
+figure
+subplot(311)
+plot(1:N,RMSEenu)
+ylabel('error(m)')
+xlabel('epoch')
+legend('E','N','U')
+title('RMSE ENU(DataSet4)')
+subplot(312)
+plot(1:N,RMSE3d)
+ylabel('error(m)')
+xlabel('epoch')
+title('RMSE 3D(DataSet4)')
+subplot(313)
+plot(1:N,RMSEclkB)
+ylabel('error(s)')
+xlabel('epoch')
+title('RMSE Clock Bias(DataSet4)')
 
+figure
+plot(1:N,(prDataP1-prDataP2))
+ylabel('error(m)')
+xlabel('epoch')
+title('Difference in observed pseudorange on L1 and L2(DataSet4)')
