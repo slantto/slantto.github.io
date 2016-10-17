0735615d50407efadb4e45a25f1876b5dd83904b
slantto <slantto@mix.wvu.edu>
2016-10-17 18:12:35 -0400

    Started modifying KF for orbit data

--- GPS Functions/carrierSmooth.m
+++ GPS Functions/carrierSmooth.m
@@ -1,9 +1,10 @@
-function prSmooth=carrierSmooth(phaseL1,phaseL2,M,prDataIF)
+function prSmooth=carrierSmooth(PhaseL1,PhaseL2,M,prDataIF)
 
-phaseIF=(2.546*phaseL1)-(1.546*phaseL2);
-prSmooth(:,1)=prDataIF(:,1);
+phaseIF=(2.546*PhaseL1)-(1.546*PhaseL2);
+phaseIF=phaseIF';
+prSmooth(1,:)=prDataIF(1,:);
 
 for j=2:length(prDataIF)
-    prSmooth(:,j)=((1/M)*prDataIF(:,j))+(((M-1)/M)*(phaseIF(:,j)-phaseIF(:,j-1)+prSmooth(:,j-1)));
+    prSmooth(j,:)=((1/M)*prDataIF(j,:))'+(((M-1)/M)*(phaseIF(:,j)-phaseIF(:,j-1)+prSmooth(j-1,:)));
 end
 end
